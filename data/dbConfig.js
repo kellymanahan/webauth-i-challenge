40ca4f175748c46dc9cb21f375d182bbbb00eb41
kammagik <kammagik@yahoo.com>
2019-04-04 23:17:16 -0400

    migrations included

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,18 @@
 const knex = require('knex');
+const dbConfig = require('../knexfile');
 
-const knexConfig = require('../knexfile.js');
+const db = knex(dbConfig.development);
 
-module.exports = knex(knexConfig.development);
+module.exports = {
+    add: function(user) {
+        return db('users').insert(user);
+    },
+
+    get: function(user) {
+        return db('users').where('username', user.username)
+    },
+    
+    findBy: () => {
+        return db('users').select("username");
+    }
+}
--- index.js
+++ index.js
@@ -2,26 +2,40 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 const db = require('./data/dbConfig.js');
-const Users = require('.data/migrations/users.js');
 
 const server = express();
+
+const sessionOptions = {
+  name:'webauthi',
+  secret: 'this is a secret',
+  cookie: {
+    maxAge: 1000* 60 * 60 * 2,
+    secure: false
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false
+}
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionOptions));
 
 server.get('/', (req, res) => {
   res.send("Welcome!");
 });
 
 server.post('/api/register', (req, res) => {
-  let user = req.body;
+  const user = req.body;
 
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
-  Users.add(user)
+  
+  db.add(user)
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -31,13 +45,12 @@ server.post('/api/register', (req, res) => {
 });
 
 server.post('/api/login', (req, res) => {
-  let { username, password } = req.body;
+  const user = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-        const hash = bcrypt.hashSync(password, 10);
-      if (user && bcrypt.compareSync(password, user.password)) {
+  db.get(user)
+    .then(users => {
+      if (user && bcrypt.compareSync(user.password, users.password)) {
+        req.session.username = users[0].username
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -49,12 +62,19 @@ server.post('/api/login', (req, res) => {
 });
 
 server.get('/api/users', (req, res) => {
-  Users.find()
+  if(req.session && req.session.username) {
+  db.findBy()
     .then(users => {
       res.json(users);
     })
-    .catch(err => res.send(err));
+    .catch(err => { 
+      res.send(err);
+    })
+  } else {
+    res.status(400).json({message: 'cannot retrieve request'})
+  }
 });
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
